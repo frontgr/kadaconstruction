c4728997ad41c45e309c8c11ef0eed5fef11206f
Aleksey02 <lyosha2002stich@gmail.com>
2024-03-07 10:00:41 +0500

    feat: Add form adaptive

--- frontend/js/formInputMask.js
+++ frontend/js/formInputMask.js
@@ -1,3 +1,22 @@
 $(document).ready(function () {
   $("#phone").inputmask({ mask: "+7 (999) 999-99-99" });
 });
+
+let input = $('.form__left-parameters-area-input');
+let send_btn = $('.form__right-send__button');
+
+function updatePlaceholder() {
+  if (window.innerWidth <= 768) {
+        input[0].placeholder = "Площадь помещения";
+    } else {
+        input[0].placeholder = "Площадь вашего помещения";
+  }
+  if(window.innerWidth <= 480) {
+    send_btn[0].value = "Отправить заявку";
+  }else{
+    send_btn[0].value = "Отправить заявку для консультации";
+  }
+}
+
+updatePlaceholder(); // вызываем функцию сразу, чтобы установить начальное значение
+window.addEventListener('resize', updatePlaceholder);
